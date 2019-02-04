a10d63192e1f4518cc0c069e5360a41f98f5038c
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-02-04 08:41:00 -0500

    Update SortCompetition.java

--- src/SortCompetition.java
+++ src/SortCompetition.java
@@ -10,22 +10,7 @@ SortCompetition {
        		arr[j] = temp;
     	}
 	
-	public static void bubbleSort(int[] arr){
-        int x = 1;
-        while (x !=0){
-            x=0;
-            for (int i=0; i<arr.length-1; i++){
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr, arr[i], arr[i+1]);
-                    x++;
-                }
-            }
-        }
-    }
-	public abstract int challengeOne(int[] arr){
-		bubbleSort(arr);
-		median(arr);
-	}
+	public abstract int challengeOne(int[] arr);
 	
 	public static int[] randomIntsArr(int num){
         	int []arr = new int[num];
@@ -36,7 +21,7 @@ SortCompetition {
     	}
 	
 	public static int median(int[] arr) {
-       		 int med = 0;
+       		 int mid = 0;
        		 if(arr.length % 2 == 0) {
         	 med = (arr[arr.length / 2 + 1] + arr[arr.length / 2]) / 2;
        	 }
@@ -64,14 +49,50 @@ SortCompetition {
 	   }
 	}
 
-	public abstract int challengeThree(int[] arr); 
-	
-	public static int[]randomNearSort(int num) {
-		int []arr = new int[num];
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = i + 1;
+	public abstract int challengeThree(int[] SortChallengeThree)
+	{
+		int mid = 0;
+       		if(Arr.length % 2 == 0) 
+		{
+			med = (Arr[Arr.length / 2 + 1] + Arr[Arr.length / 2]) / 2;
 		}
-		return arr;
+		else
+		{
+			med = Arr[Arr.length / 2];
+		}
+		return med;
+	}
+	
+	public static int[] randomNearSort(int num) 
+	{
+		randomIntsArr(num);
+		for(int i = 0; i < 80000; i++) 
+		{
+			for (int n = 0; n < Arr.length - 1; n++) 
+			{
+				if (Arr[n] > Arr[n + 1]) 
+				{
+					swap(Arr, n, n + 1);
+				}
+			}
+		}
+		return Arr;
+	}
+	
+	public static int[] SortChallengeThree(int num);
+	{
+		randomNearSort(num);
+		for(int x = 1; x < Arr.length; x++)
+		{
+			for(int y = x; y > 0; y--)
+			{
+				if (intArr[y] < Arr[y-1])
+				{
+					swap(Arr, y,y-1);
+				}
+			}
+		}
+		return Arr;
 	}
 
 	public abstract int challengeFour(int[][] arr);
